04aa588e686c070b11efadf3cef39b6ccfaa3eda
pond6 <ikeda@framelunch.jp>
2016-04-11 17:43:09 +0900

    add utils/degree test

---
+++ test/spec/utils/degree.spec.js
@@ -0,0 +1,13 @@
+var degree = require('../../../lib/utils/degree');
+
+describe('degree', function () {
+    it('should angle 0 is degree 0', function () {
+        expect(degree(0)).toEqual(0);
+    });
+    it('should angle 90 is degree Math.PI/2', function () {
+        expect(degree(90)).toEqual(Math.PI/2);
+    });
+    it('should angle 180 is degree Math.PI', function () {
+        expect(degree(180)).toEqual(Math.PI);
+    });
+});
